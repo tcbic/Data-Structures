4fce708aeec10e28c1d8c7d1c0da29dabfd15cc0
Taylor <bickell.taylor@gmail.com>
2020-01-27 22:59:23 -0800

    Day 1, Data Structures

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,35 @@
-import sys
-sys.path.append('../doubly_linked_list')
+# import sys
+# sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+# Characteristics
+# -linear data structure
+# -flexible size
+# -FIFO (First In First Out)- Think about a line of people waiting to get into 
+# a movie theatre.
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Complexity of 1 (Constant) to add to the front or back.
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        """Add a node to the tail."""
+        # The size of the queue increases by 1.
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        """Remove a node from the head."""
+        if self.size > 0:
+            # The size of the queue decreases by 1.
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else:
+            return None
 
     def len(self):
-        pass
+        """Return the length/size of the queue."""
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,34 @@
-import sys
-sys.path.append('../doubly_linked_list')
+# import sys
+# sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+
+# Characteristics
+# -linear data structure
+# -flexible size
+# LIFO (Last In First Out)- Think about a stack of plates.
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        #
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        """Add a new node to the top of the stack."""
+        # Size of the stack increases by 1.
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        """Removes a node from the top of the stack."""
+        if self.size > 0:
+            # Size of the stack decreases by 1.
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else:
+            return None
 
     def len(self):
-        pass
+        """Return the length/size of the stack."""
+        return self.size
